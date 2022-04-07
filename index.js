ca2da7d2dbe6e50643c7386b3d1d088385386157
letotero <lferreiranunes@outlook.com>
2022-04-07 00:48:59 -0300

    Arquivo adicionado

---
+++ index.js
@@ -0,0 +1,64 @@
+//Exercício 1
+// let nomeCompleto = "Leticia Ferreira Nunes";
+
+//   console.log(nomeCompleto);
+
+//Exercício 2
+// const numeroA = 7;
+// const numeroB = 4;
+// const resultado = numeroA * numeroB;
+
+// console.log(resultado);
+
+// Exercício 3
+// function saudacao() {
+// 	return console.log("Oi eu sou o Goku");
+// };
+
+//  saudacao();
+
+//Exercício 4
+// function multiplica(vegeta, goku) {
+// 	return console.log(vegeta * goku);
+// }
+
+// multiplica(4, 3);
+
+//Exercício 5
+//  function podeDirigir(idade) {
+//  if (idade >= 18) {
+//  	return console.log("Você pode dirigir")
+//  } else {
+//  	return console.log("Você não pode dirigir");
+//  }
+// }
+
+//   podeDirigir(17);
+
+//Exercício 6
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+//Exercício 7
+// for (let i=0; i <= 20; i++) {
+//  if (i % 2 !== 0) {
+//  	console.log(i)
+//  }
+// }
+
+//Exercício 8
+// function tabuada(valor) {
+// 	for(let i = 0; i <= 10; i++) {
+// 		console.log(valor * i)
+// 	}
+// }
+
+// tabuada(9)
+
+//Exercício 9
+function converterEmHoras(minutos) {
+	return console.log(minutos/60)
+}
+
+converterEmHoras(240)
